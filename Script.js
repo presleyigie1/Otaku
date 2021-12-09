21d811e998df88ea0491837c5be713720b9f9a8c
KxngSSJ <x21383536@student.ncirl.ie>
2021-12-09 13:00:38 +0000

    Updated

---
+++ Script.js
@@ -0,0 +1,17 @@
+/*solomon*/
+var status = "less";
+
+function toggleText()
+{
+    var text="Lmao";
+
+    if (status == "less") {
+        document.getElementById("textArea").innerHTML=text;
+        document.getElementById("toggleButton").innerText = "See Less";
+        status = "more";
+    } else if (status == "more") {
+        document.getElementById("textArea").innerHTML = "";
+        document.getElementById("toggleButton").innerText = "See More";
+        status = "less"
+    }
+}
