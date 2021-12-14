947fb10f2b6655bb14bd20c1384b7a242c545ef1
Sean-Robert <91732889+SanRob1325@users.noreply.github.com>
2021-12-14 21:33:34 +0000

    Added new content

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+
 //Sean
  function guessNum(){
 
@@ -22,3 +23,61 @@
         window.alert("This is not valid,ERROR!"); //This message prints if the value is not an integer
     }
 }
+
+//presley-JavaScript fr contact page
+function validate(){
+  var formData = []; // array that holds data from the form fields
+  // arrays count from 0 up
+  formData[0] = document.getElementById("nameData").value;
+  formData[1] = document.getElementById("emailData").value;
+  formData[2] = document.getElementById("msgData").value;
+
+  if(formData[0] .length == ""){
+    document.getElementById("nameFeedback").innerHTML = "This field is required"
+    document.getElementById("nameFeedback").style.visibility = "visible";
+    document.getElementById("nameFeedback").style.color="red";
+  }
+    else if(formData[0] .length < 2){
+      document.getElementById("nameFeedback").innerHTML = "Name Is Not Valid"
+      document.getElementById("nameFeedback").style.visibility = "visible";
+  }
+
+  var symbol = formData[1].indexOf("@");
+
+  if(symbol = 0){
+    document.getElementById("errorText").innerHTML = "Please enter a valid email address";
+    document.getElementById("errorText").style.visibility = "visible";
+    document.getElementById("errorText").style.color="red";
+  }
+    else if(formData[1] .length == 2 || formData[1] .length == 1){
+      document.getElementById("errorText").innerHTML = "Please enter a valid email address";
+      document.getElementById("errorText").style.visibility = "visible";
+      document.getElementById("errorText").style.color="red";
+    }
+      else if(formData[1] .length == "" ){
+        document.getElementById("errorText").innerHTML = "This field is required";
+        document.getElementById("errorText").style.visibility = "visible";
+        document.getElementById("errorText").style.color="red";
+      }
+
+    else{
+        //if it is there then capitalise the email & show user
+          document.getElementById("emailFeedback").innerHTML = input.toUpperCase();
+  }
+    if(formData[2] .length == "" ){
+      document.getElementById("msgFeedback").innerHTML = "This field is required"
+      document.getElementById("msgFeedback").style.visibility = "visible";
+      document.getElementById("msgFeedback").style.color="red";
+
+  }
+
+}
+
+//presley: source - https://www.youtube.com/watch?v=ydZc17rlR5E&t=20s -Florin pop
+  const hamburger = document.getElementById('hamburger');
+  const nav = document.getElementById('nav-ul');
+
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('show');
+  });
+
