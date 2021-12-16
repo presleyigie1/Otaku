8edece2cc17d06de865e4808d6da07fe027646b1
presleyigie1 <presleyigie3@gmail.com>
2021-12-16 18:10:13 +0000

    Finished media queries on SAO and Contact Us page

--- Script.js
+++ Script.js
@@ -1,3 +1,61 @@
+//Sean
+ function guessNum(){
+
+     var randNum = Math.floor(Math.random() * 20); //Uses randomisation,Uses number only from 1-10,it only returns integer values of this range
+
+     //This produces an output of the random number-Sean
+    document.write(randNum+" " +"This is the number generated!");
+
+    var userInput= prompt("Enter any number between 1-20");
+    //User inputs number ("Enter any number between 1-30+randNum")//Sean
+
+    if(userInput === randNum){                  //Sean
+        window.alert("Correct! you are right!");// this message will pop up if you guessed the number correctly
+
+    }else if(userInput > randNum){                          //Sean
+        window.alert(" You guessed it too low,you lose!");//This message pops up if you have guess it too low
+    }
+     else if (userInput < randNum){                         //Sean
+        window.alert("You guessed it too high,you lose!");//if number us guessed too high this alert will pop up
+    }
+    else if(randNum >= 30){
+        window.alert("You cannot enter anything above 20");
+    }
+    else if(randNum <= 1){                                  //Sean
+        window.alert("You cannot enter anything below 1");//if number is not in the correct range of guessing
+    }
+    else{                                           //Sean
+        window.alert("This is not valid,ERROR!"); //This message prints if the value is not an integer
+    }
+}
+
+
+//Sean
+ function guessNum(){
+
+     var randNum = Math.floor(Math.random() * 30); //Uses randomisation,Uses number only from 1-10,it only returns integer values of this range
+
+     //This produces an output of the random number
+    print(randNum);
+
+    var userInput= prompt("Enter any number between 1-30");
+    //User inputs number ("Enter any number between 1-30+randNum")
+
+    if(userInput < randNum){
+        window.alert("Correct! you are right!");// this message will pop up if you guessed the number correctly
+
+    }else if(userInput > randNum){
+        window.alert(" You guessed it too low,you lose!");//This message pops up if you have guess it toow
+    }
+     else if (userInput == randNum){
+        window.alert("You guessed it too high,you lose!");
+    }
+    else{
+        window.alert("This is not valid,ERROR!"); //This message prints if the value is not an integer
+    }
+}
+
+
 //presley-JavaScript for contact page
 function validate(){
   var formData = []; // array that holds data from the form fields
@@ -58,9 +116,11 @@ hamburger.addEventListener('click', () => {
 nav.classList.toggle('show');
 });
 
-/*this was the first dom manipulation i was going to use but i couldnt find a way to make the color change to something else, then i decided to take the light switch/ dark and light concept and implemented it into the JS code below that chnages the background cover and makes the button hidden and visible
+/*this was the first dom manipulation i was going to use but i couldnt find a way to make the color change back to its original state, then i decided to use the light switch/ dark and light concept and implement it into the JS code below that chnages the background cover and makes the button hidden and visible
 
-//DOM MANIPULATION source: https://www.youtube.com/watch?v=jIyBx0Yh4rI
+INITIAL CODE BELOW :
+
+DOM MANIPULATION source: https://www.youtube.com/watch?v=jIyBx0Yh4rI
 document.getElementById("appearance").onclick = changeBackgroundColorToGreen;
 document.getElementById("appearance").onclick = changeBackgroundColorToYellow;
 
@@ -68,7 +128,7 @@ function changeBackgroundColorToGreen(){
 document.querySelector("body").style.background = "green";
 } */
 
-
+// DOM MANIPULATION - USING A TBACKHGROUND COLOUR SWITCHER
 
 function changeBackgroundColor(){
   //if the background color is not green and you click the moon icon, the background will change to green and hide the moon icon and make the sun icon visible
